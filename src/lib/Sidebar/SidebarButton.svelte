6f037d29dc2589b9986c1a9c7721e592efbf7a5f
Liam Greenway <ApplePiePc@outlook.com>
2025-04-10 19:50:47 -0400

    Update

--- src/lib/Sidebar/SidebarButton.svelte
+++ src/lib/Sidebar/SidebarButton.svelte
@@ -5,7 +5,9 @@
 <li class="ml-2 my-5">
 	<a href={href}>
 		<button class="bg-gray-600  w-60 	dark:bg-gray-800  text-left text-2xl dark:text-white text-black">
-				{ text }
+				<p class="dark:text-white">
+					{ text }
+				</p>
 		</button>
 	</a>
 </li>
---
+++ src/routes/authcallback/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { onMount } from 'svelte';
+	import { getLoggedInUserId, supabase } from '$lib';
+	import { generateFromEmail } from 'unique-username-generator';
+
+	onMount(async () => {
+		let user_id = await getLoggedInUserId();
+		let auth = await supabase.auth.getUser();
+
+		let {data} = await supabase.from('users').insert({id: user_id, username: generateFromEmail(auth.data?.user?.email ?? "example@email.com"), display_name: "New User", karma: 0}).select();
+	})
+</script>
